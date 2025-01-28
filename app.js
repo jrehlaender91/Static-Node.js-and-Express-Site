410f4c2a5a202decfb55b8fa64e2ed1bbb38fa7f
jrehlaender91 <73409365+jrehlaender91@users.noreply.github.com>
2025-01-28 15:54:57 -0600

    V1.0.2
    Finished first version

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ app.get('/about', (req, res) => {
 app.get('/projects/:id', (req, res) => {
     const id = req.params.id;
     res.render('project', {
-        project: projects[req.params.id].project_name
+        project: projects[req.params.id].project_name,
+        description: projects[req.params.id].description,
+        technologies: projects[req.params.id].technologies,
+        live: projects[req.params.id].live_link,
+        github: projects[req.params.id].github_link,
+        images: projects[req.params.id].image_urls.slice(1)
     });
 });
 
@@ -32,8 +37,13 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
     res.locals.error = err;
-    res.status(err.status);
-    res.render('error');
+    if(err.status === 404) {
+        res.status(404);
+        res.render('page-not-found', err);
+    } else {
+        res.status(err.status);
+        res.render('error', err);
+    }
 });
 
 app.listen(port);
