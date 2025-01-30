8fdc8b2fac38b7f421e454c880d798d56b53b840
jrehlaender91 <73409365+jrehlaender91@users.noreply.github.com>
2025-01-29 19:26:09 -0600

    V1.0.3
    Added README.md and fixed a couple of error handling bugs.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Portfolio (Static Node.js and Express Site)
+
+This is a project that shows my portfolio built with Node.js and Express.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+Instructions on how to install and set up the project.
+
+```bash
+npm install
+```
+
+## Usage
+
+Instructions on how to use the project.
+
+```bash
+npm start
+```
+
+## Changes
+Charges to the styles include:
+- Changed the main font to Roboto.
+- Body and sidebar background color to #a1e1fb
+- Larger font-size to the Technologies list.
+- Changed the color of the Live Demo and Github to #cdcdcd; and added a hover effect with a transition.
+- Added a hover effect with a transition to the "Learn More" link.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- app.js
+++ app.js
@@ -17,20 +17,31 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/projects/:id', (req, res) => {
+app.get('/projects/:id', (req, res, next) => {
+    const numProjects = projects.length - 1;
     const id = req.params.id;
-    res.render('project', {
-        project: projects[req.params.id].project_name,
-        description: projects[req.params.id].description,
-        technologies: projects[req.params.id].technologies,
-        live: projects[req.params.id].live_link,
-        github: projects[req.params.id].github_link,
-        images: projects[req.params.id].image_urls.slice(1)
-    });
+    console.log(isNaN("l"));
+    if (id > numProjects || isNaN(id)) {
+        const err = new Error('Sorry, the project you are looking for does not exist.');
+        err.status = 404;
+        console.log(err);
+        next(err);
+    }
+    else {
+        res.render('project', {
+            project: projects[req.params.id].project_name,
+            description: projects[req.params.id].description,
+            technologies: projects[req.params.id].technologies,
+            live: projects[req.params.id].live_link,
+            github: projects[req.params.id].github_link,
+            images: projects[req.params.id].image_urls.slice(1)
+        });
+    }
 });
 
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
+    const err = new Error('Sorry, the page you are looking for does not exist.');
+    console.log(err);
     err.status = 404;
     next(err);
 });
